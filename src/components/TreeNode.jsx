bdd44866344ea361f6a48b84c0698cc54bf69ecb
Hubert Kuch <kuchhubert@gmail.com>
2025-10-25 12:38:50 +0200

    feat: proicons

--- src/components/TreeNode.jsx
+++ src/components/TreeNode.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FolderIcon, FolderOpenIcon } from '@proicons/react';
 
 function TreeNode({ node }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,11 +11,7 @@ function TreeNode({ node }) {
   return (
     <div className="ml-4 w-full text-left items-start content-start">
       <button onClick={handleToggle} type="button" className="block pointer btn btn-sm btn-ghost mr-1 text-left w-full">
-        {node.children && (
-          <span>
-            {isOpen ? '[-]' : '[+]'}
-          </span>
-        )}
+        {node.children && (isOpen ? <FolderOpenIcon className="inline" /> : <FolderIcon className="inline" />)}
         <span>{node.name}</span>
       </button>
       {isOpen && node.children && (
